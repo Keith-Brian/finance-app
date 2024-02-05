6d5db2870372f04f6d38b4de3027b982fd7378dc
keith-brian <chiefdeveloper254@gmail.com>
2024-02-05 23:39:26 +0300

    Added Wallet Section

--- app/src/main/java/com/chiefdeveloper/financeapp/HomeScreen.kt
+++ app/src/main/java/com/chiefdeveloper/financeapp/HomeScreen.kt
@@ -2,16 +2,24 @@ package com.chiefdeveloper.financeapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -77,7 +86,44 @@ fun getGradient(
     )
 }
 
+@Composable
+@Preview
+fun WalletSection() {
 
+
+    Row(modifier = Modifier.fillMaxWidth()
+        .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically){
+
+        Column {
+            Text(text = "Hi Keith",
+                color = Color.Black,
+                fontWeight = FontWeight.Light,
+                fontSize = 20.sp)
+
+            Text(text = "Balance $ ${30.67}",
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp)
+        }
+
+        Box(
+            Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .size(40.dp)
+                .clickable {  }
+                .background(MaterialTheme.colorScheme.secondaryContainer),
+            contentAlignment = Alignment.Center
+        ){
+            Icon(imageVector = Icons.Filled.Notifications,
+                contentDescription = "Notification",
+                tint = MaterialTheme.colorScheme.onSecondaryContainer)
+        }
+
+    }
+
+}
 
 @Composable
 @Preview
--- app/src/main/java/com/chiefdeveloper/financeapp/MainActivity.kt
+++ app/src/main/java/com/chiefdeveloper/financeapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.chiefdeveloper.financeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chiefdeveloper.financeapp.ui.theme.FinanceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   CardRow()
+                   Column {
+
+                       WalletSection()
+                       Spacer(modifier = Modifier.height(10.dp))
+                       CardRow()
+
+                   }
                 }
             }
         }
@@ -35,6 +45,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     FinanceAppTheme {
-       CardRow()
+        Column {
+
+            WalletSection()
+            Spacer(modifier = Modifier.height(10.dp))
+            CardRow()
+
+        }
     }
 }
