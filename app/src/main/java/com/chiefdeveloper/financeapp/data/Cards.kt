38bf2ec5975922f7a7521d4b84905047151708ad
keith-brian <chiefdeveloper254@gmail.com>
2024-02-05 20:03:08 +0300

    first commit

---
+++ app/src/main/java/com/chiefdeveloper/financeapp/data/Cards.kt
@@ -0,0 +1,8 @@
+package com.chiefdeveloper.financeapp.data
+
+data class Cards(
+    val cardType: String,
+    val accountType: String,
+    val accountBalance: String,
+    val cardNumber: String
+)
